1ec2bd31fc01f8fd44c1ef59d5a58f4566854e69
RK-404 <rizwan_k@RIZWANs-Air.lan>
2024-01-02 19:17:00 -0500

    Solved

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const addCounter = () => {
+    setCount(count + 1);
+  }
+
+  const multiplyCounter = () => {
+    setCount(count * 2);
+  }
+
   return (
     <div className="App">
       <div className="number">{count}</div>
       <div className="buttons-container">
-        <div className="counter-button">+1</div>
-        <div className="counter-button blue">x2</div>
+        <div className="counter-button" onClick={addCounter}>+1</div>
+        <div className="counter-button blue" onClick={multiplyCounter}>x2</div>
       </div>
     </div>
   );
